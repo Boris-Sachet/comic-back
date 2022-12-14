53264480d9aa8dbf5a3f602f445099d1194739f2
Boris Sachet <boris.sachet@gmail.com>
2022-12-14 09:13:10 +0000

    fixed compose-up script

--- compose-up.sh
+++ compose-up.sh
@@ -1,3 +1,4 @@
 #/bin/bash
-docker-compose -f ./docker/docker-compose.yml --env-file ./docker/.env build --pull
+docker-compose -f ./docker/docker-compose.yml --env-file ./docker/.env down
+docker-compose -f ./docker/docker-compose.yml --env-file ./docker/.env pull comic_back
 docker-compose -f ./docker/docker-compose.yml --env-file ./docker/.env up -d
