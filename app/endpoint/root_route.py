0d80b536b9a10ef798dc384cfc7c271b36168e3d
Boris SACHET <boris.sachet@gmail.com>
2023-02-10 21:34:19 +0100

    Fixed ping route add quotes

--- app/endpoint/root_route.py
+++ app/endpoint/root_route.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
+from fastapi.responses import PlainTextResponse
 
 router = APIRouter(tags=["Root"])
 
 
-@router.get("/ping")
+@router.get("/ping", response_class=PlainTextResponse)
 async def ping():
     """Anyone home?"""
-    return "pong"
-
+    return 'pong'
